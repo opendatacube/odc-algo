eed21c3a735886d235daca8da6eaf0f0bb0d9c69
Damien Ayers <damien@omad.net>
2023-10-10 17:18:19 +1100

    Rust debug builds of geomedian are too slow for testing

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 from setuptools_rust import RustExtension, Strip
 
 
+# Never build rust debug version, it's too slow to do anything
 setup(
     packages=find_packages(),
-    rust_extensions=[RustExtension("odc.algo.backend", strip=Strip.No)]
+    rust_extensions=[RustExtension("odc.algo.backend", 
+                                   strip=Strip.No,
+                                   debug=False)]
 )
